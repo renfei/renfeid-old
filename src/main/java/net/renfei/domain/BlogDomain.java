ad0185f1c17e371057bfff41b9b98908e800ccad
renfei <i@renfei.net>
2021-11-19 17:03:23 +0800

    完善博客领域对象

--- src/main/java/net/renfei/domain/BlogDomain.java
+++ src/main/java/net/renfei/domain/BlogDomain.java
@@ -1,5 +1,11 @@
 package net.renfei.domain;
 
+import net.renfei.domain.comment.Comment;
+import net.renfei.domain.system.SystemTypeEnum;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * 博客领域的聚合根
  * 与博客相关的领域对象都在这里
@@ -7,7 +13,9 @@ package net.renfei.domain;
  * @author renfei
  */
 public final class BlogDomain {
-    public static BlogDomain getBlogById(Long id) {
-        return null;
+    private final List<Comment> commentList;
+
+    public BlogDomain(Long id) {
+        commentList = new CommentDomain(SystemTypeEnum.BLOG, id).getCommentList();
     }
 }
