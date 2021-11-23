8bf4cb82c65ac6fc5b0a04a0d47cbf7b45ffbf54
RenFei <i@renfei.net>
2021-11-23 22:21:16 +0800

    添加邮件模板

--- src/main/java/net/renfei/services/EmailService.java
+++ src/main/java/net/renfei/services/EmailService.java
@@ -4,9 +4,17 @@ import java.io.File;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 电子邮件发送服务
+ *
+ * @author renfei
+ */
 public interface EmailService {
     boolean send(String to, String name, String subject, List<String> contents);
+
     boolean send(String to, String name, String subject, String contents);
+
     boolean send(String to, String name, String subject, List<String> contents, Map<String, File> attachment);
+
     boolean send(String to, String name, String subject, String contents, Map<String, File> attachment);
 }
