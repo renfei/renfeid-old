5f93cc9b3bf15944b512a32767b541fd7751514e
renfei <i@renfei.net>
2021-11-25 17:38:35 +0800

    fix: Test SpringBootTest

--- src/test/java/net/renfei/ApplicationTests.java
+++ src/test/java/net/renfei/ApplicationTests.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.containsString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,16 +13,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
 @AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
     @Autowired
     protected MockMvc mockMvc;
 
     @Test
     void getActive() throws Exception {
-        this.mockMvc.perform(get("/test/active")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("dev")));
+        this.mockMvc.perform(get("/test/active"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
     }
 
 }
