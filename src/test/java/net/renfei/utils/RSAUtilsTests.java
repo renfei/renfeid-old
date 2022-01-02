d54f8dbfa4c17e177837f24e8405bb5462b3914a
RenFei <i@renfei.net>
2022-01-02 15:45:30 +0800

    添加 RSA 非对称加解密工具类

---
+++ src/test/java/net/renfei/utils/RSAUtilsTests.java
@@ -0,0 +1,25 @@
+package net.renfei.utils;
+
+import net.renfei.ApplicationTests;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+/**
+ * @author renfei
+ */
+public class RSAUtilsTests extends ApplicationTests {
+    @Test
+    public void test() throws Exception {
+        String msg = "tester";
+        //0表示公钥
+        //1表示私钥
+        Map<Integer, String> data = RSAUtils.genKeyPair(1024);
+        //RSA公钥加密
+        String encrypt = RSAUtils.encrypt(msg, data.get(0));
+        assert msg.equals(RSAUtils.decrypt(encrypt, data.get(1)));
+        //RSA私钥加密
+        encrypt = RSAUtils.encryptByPrivateKey(msg, data.get(1));
+        assert msg.equals(RSAUtils.decryptByPublicKey(encrypt, data.get(0)));
+    }
+}
