703e7593ad256f34ec8f655558275f8de9f8f159
RenFei <i@renfei.net>
2022-01-02 18:55:21 +0800

    调整 JWT 工具类功能

--- src/main/java/net/renfei/utils/JwtTokenUtils.java
+++ src/main/java/net/renfei/utils/JwtTokenUtils.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 /**
@@ -17,23 +18,21 @@ import java.util.*;
  * @author RenFei(i @ renfei.net)
  */
 @Service
-public class JwtUtils {
+public class JwtTokenUtils {
     private final SystemConfig SYSTEM_CONFIG;
 
-    public JwtUtils(SystemConfig systemConfig) {
+    public JwtTokenUtils(SystemConfig systemConfig) {
         this.SYSTEM_CONFIG = systemConfig;
     }
 
     /**
      * 签发 JWT
      *
-     * @param userId      用户UUID
-     * @param authorities 权限列表
-     * @param roles       角色列表
-     * @param subject     可以是 JSON 数据，尽可能少
+     * @param userName 用户名
+     * @param request  请求对象
      * @return
      */
-    public String createJWT(String userId, List<String> roles, List<String> authorities, String subject) {
+    public String createJWT(String userName, HttpServletRequest request) {
         SignatureAlgorithm algorithm = SignatureAlgorithm.HS256;
         long nowMillis = System.currentTimeMillis();
         Date now = new Date(nowMillis);
@@ -49,19 +48,14 @@ public class JwtUtils {
         // jti (JWT ID)，防止jwt被重新发送
         claims.setId(UUID.randomUUID().toString())
                 // 主题
-                .setSubject(subject)
+                .setSubject(userName)
+                // 受众，跟IP绑定
+                .setAudience(IpUtils.getIpAddress(request))
                 // 签发时间
                 .setIssuedAt(now)
                 // 签发者
                 .setIssuer(SYSTEM_CONFIG.getJwt().getIssuer());
 
-        // payload 中 放入自定义信息
-        Map<String, Object> selfMap = new HashMap<>(3);
-        selfMap.put("user_id", userId);
-        selfMap.put("roles", roles);
-        selfMap.put("authorities", authorities);
-        claims.putAll(selfMap);
-        // header
         JwtBuilder builder = Jwts.builder().setHeader(map)
                 // 使用 JSON 实例设置 payload
                 .setClaims(claims)
@@ -71,11 +65,6 @@ public class JwtUtils {
         // 过期时间
         builder.setExpiration(expDate);
         return builder.compact();
-    }
-
-    private SecretKey generalKey() {
-        byte[] encodedKey = Base64.getDecoder().decode(SYSTEM_CONFIG.getJwt().getSecret());
-        return new SecretKeySpec(encodedKey, 0, encodedKey.length, "HmacSHA256");
     }
 
     /**
@@ -97,5 +86,36 @@ public class JwtUtils {
         } catch (Exception ignored) {
         }
         return claims;
+    }
+
+    /**
+     * 获取用户名
+     *
+     * @param token
+     * @return
+     */
+    public String getUsername(String token) {
+        Claims claims = parseJWT(token);
+        return claims == null ? null : claims.getSubject();
+    }
+
+    /**
+     * TOKEN 与IP进行绑定校验
+     *
+     * @param token
+     * @param request
+     * @return
+     */
+    public boolean validate(String token, HttpServletRequest request) {
+        Claims claims = this.parseJWT(token);
+        if (this.parseJWT(token) == null) {
+            return false;
+        }
+        return IpUtils.getIpAddress(request).equals(claims.getAudience());
+    }
+
+    private SecretKey generalKey() {
+        byte[] encodedKey = Base64.getDecoder().decode(SYSTEM_CONFIG.getJwt().getSecret());
+        return new SecretKeySpec(encodedKey, 0, encodedKey.length, "HmacSHA256");
     }
 }
