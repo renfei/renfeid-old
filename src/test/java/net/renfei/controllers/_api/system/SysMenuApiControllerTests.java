ecdbf1f05986c9335577797f3c1641384338e81f
RenFei <i@renfei.net>
2022-01-28 18:57:44 +0800

    ci: 增加单元测试覆盖

--- src/test/java/net/renfei/controllers/_api/system/SysMenuApiControllerTests.java
+++ src/test/java/net/renfei/controllers/_api/system/SysMenuApiControllerTests.java
@@ -26,6 +26,30 @@ public class SysMenuApiControllerTests extends ApplicationTests {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("$.code").value(200));
+        // 测试普通用户的
+        SignInVO signInVO = new SignInVO();
+        signInVO.setKeyUuid("a3c2a92a-08ce-4598-80ef-55e0366b4484");
+        signInVO.setUserName(USER_NAME);
+        signInVO.setPassword("QSZv+HMaiJLT6YFS8yZrlg==");
+        signInVO.setReCAPTCHAToken("03AGdBq26c-0yL1qQkAXinEUyCsN24-FMSKQFUzWe0VpxS0Uy4odaoaM-0j5_bSekK2RPleQp7mtrReoqh-JDBCCcziTOebXeQ7McgJYbsb4qQJFWJywBtJerDIdGqjojB91WHk5VfLWlLvL1I90rnDw_BIoKAdy4K60bnCvBGBF8W_vj9vLsn5cXSrF_fyYsSbb2OHS4H1TKbEKZXtyz8ByTm174RsOCItupc4JXRIeYGJUG41bhdwBnkIwNX4R9FdLes_0Ah7n_13W9b82fJ20001O2bOLTofcYXhf0jjQytdj0olW2HfCK4-sq2GLOn1GTWu0IIb870MuW3JWAByDc-jdoAeNEXQFL_E-feDfm9Y5aBgb_sbI0Id-Nn8wVOrgJZr1GEGuoTNshiV9v_yNQDfc3Gv5kof2FdphH_nkt9CqJXVVrheK1724ZFPt0ZVPu9iN0fJYGhBwLGTJhFvyuL6XVVdjEMjQ");
+        signInVO.setUseVerCode(false);
+        signInVO.settOtp("");
+        this.mockMvc.perform(post("/-/api/auth/signIn")
+                        .session(session)
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(JacksonUtil.obj2String(signInVO)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200));
+        this.mockMvc.perform(get("/_/api/sys/menu")
+                        .session(session)
+                        .param("pages", "1")
+                        .param("rows", "10"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.code").value(200));
     }
 
     @Test
@@ -75,6 +99,30 @@ public class SysMenuApiControllerTests extends ApplicationTests {
         SignInVO signInVO = new SignInVO();
         signInVO.setKeyUuid("a3c2a92a-08ce-4598-80ef-55e0366b4484");
         signInVO.setUserName(SYS_SSO);
+        signInVO.setPassword("QSZv+HMaiJLT6YFS8yZrlg==");
+        signInVO.setReCAPTCHAToken("03AGdBq26c-0yL1qQkAXinEUyCsN24-FMSKQFUzWe0VpxS0Uy4odaoaM-0j5_bSekK2RPleQp7mtrReoqh-JDBCCcziTOebXeQ7McgJYbsb4qQJFWJywBtJerDIdGqjojB91WHk5VfLWlLvL1I90rnDw_BIoKAdy4K60bnCvBGBF8W_vj9vLsn5cXSrF_fyYsSbb2OHS4H1TKbEKZXtyz8ByTm174RsOCItupc4JXRIeYGJUG41bhdwBnkIwNX4R9FdLes_0Ah7n_13W9b82fJ20001O2bOLTofcYXhf0jjQytdj0olW2HfCK4-sq2GLOn1GTWu0IIb870MuW3JWAByDc-jdoAeNEXQFL_E-feDfm9Y5aBgb_sbI0Id-Nn8wVOrgJZr1GEGuoTNshiV9v_yNQDfc3Gv5kof2FdphH_nkt9CqJXVVrheK1724ZFPt0ZVPu9iN0fJYGhBwLGTJhFvyuL6XVVdjEMjQ");
+        signInVO.setUseVerCode(false);
+        signInVO.settOtp("");
+        this.mockMvc.perform(post("/-/api/auth/signIn")
+                        .session(session)
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(JacksonUtil.obj2String(signInVO)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200));
+        this.mockMvc.perform(get("/_/api/sys/menu")
+                        .session(session)
+                        .param("pages", "1")
+                        .param("rows", "10"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.code").value(200));
+        // 测试普通用户的
+        signInVO = new SignInVO();
+        signInVO.setKeyUuid("a3c2a92a-08ce-4598-80ef-55e0366b4484");
+        signInVO.setUserName(USER_NAME);
         signInVO.setPassword("QSZv+HMaiJLT6YFS8yZrlg==");
         signInVO.setReCAPTCHAToken("03AGdBq26c-0yL1qQkAXinEUyCsN24-FMSKQFUzWe0VpxS0Uy4odaoaM-0j5_bSekK2RPleQp7mtrReoqh-JDBCCcziTOebXeQ7McgJYbsb4qQJFWJywBtJerDIdGqjojB91WHk5VfLWlLvL1I90rnDw_BIoKAdy4K60bnCvBGBF8W_vj9vLsn5cXSrF_fyYsSbb2OHS4H1TKbEKZXtyz8ByTm174RsOCItupc4JXRIeYGJUG41bhdwBnkIwNX4R9FdLes_0Ah7n_13W9b82fJ20001O2bOLTofcYXhf0jjQytdj0olW2HfCK4-sq2GLOn1GTWu0IIb870MuW3JWAByDc-jdoAeNEXQFL_E-feDfm9Y5aBgb_sbI0Id-Nn8wVOrgJZr1GEGuoTNshiV9v_yNQDfc3Gv5kof2FdphH_nkt9CqJXVVrheK1724ZFPt0ZVPu9iN0fJYGhBwLGTJhFvyuL6XVVdjEMjQ");
         signInVO.setUseVerCode(false);
