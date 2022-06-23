44bd1d826ec91d4fc66651197c462a5ba6569753
RenFei <i@renfei.net>
2022-06-23 20:08:11 +0800

    feat(vue): 添加谷歌统计和百度统计

--- renfeidvue/nuxt.config.js
+++ renfeidvue/nuxt.config.js
@@ -1,72 +1,79 @@
-
-
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: '%s - 任霏的个人网站与博客',
-    title: '任霏',
-    htmlAttrs: {
-      lang: 'zh'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        titleTemplate: '%s - 任霏的个人网站与博客',
+        title: '任霏',
+        htmlAttrs: {
+            lang: 'zh'
+        },
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: ''},
+            {name: 'format-detection', content: 'telephone=no'}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: 'https://cdn.renfei.net/favicon.ico'}
+        ],
+        script: [
+            {src: 'https://hm.baidu.com/hm.js?5a2b6f0a4ddf19de41cf4b646de45532'}
+        ]
     },
-    meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: ''},
-      {name: 'format-detection', content: 'telephone=no'}
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+        {src: '@/plugins/axios', mode: 'server'},
+        // {src: '@/plugins/baidu-tongji.js', mode: 'client'},
+        {src: '@/plugins/renfeidvue.js', mode: 'client'},
+        {src: '@/plugins/vue-google-adsense', mode: 'client'},
+        {src: '@/plugins/vue-kinesis', mode: 'client'},
+        {src: '@/plugins/vuetify-datetime-picker', mode: 'client'},
     ],
-    link: [
-      {rel: 'icon', type: 'image/x-icon', href: 'https://cdn.renfei.net/favicon.ico'}
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    {src: '@/plugins/renfeidvue.js',ssr: false},
-    {src: '@/plugins/vue-google-adsense', ssr: true},
-    {src: '@/plugins/vuetify-datetime-picker', ssr: false},
-    {src: '@/plugins/vue-kinesis', mode: 'client'},
-    {src: '@/plugins/axios', ssr: true},
-  ],
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/typescript
+        '@nuxt/typescript-build',
+        // https://go.nuxtjs.dev/vuetify
+        '@nuxtjs/vuetify',
+        '@nuxtjs/google-analytics',
+    ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        '@nuxtjs/axios',
+        '@nuxtjs/sentry',
+    ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-  ],
+    sentry: {
+        dsn: 'https://91b535ad631140fa9988efa8729891fa@o1071939.ingest.sentry.io/6446863',// Enter your project's DSN here
+        // Additional Module Options go here
+        // https://sentry.nuxtjs.org/sentry/options
+        tracesSampleRate: 0.8,
+        config: {
+            // Add native Sentry config here
+            // https://docs.sentry.io/platforms/javascript/guides/vue/configuration/options/
+        }
+    },
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/sentry',
-  ],
+    // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+    vuetify: {
+        customVariables: ['~/assets/variables.scss'],
+        treeShake: true,
+        optionsPath: './vuetify.options.js',
+    },
 
-  sentry:{
-    dsn: 'https://91b535ad631140fa9988efa8729891fa@o1071939.ingest.sentry.io/6446863',// Enter your project's DSN here
-    // Additional Module Options go here
-    // https://sentry.nuxtjs.org/sentry/options
-    tracesSampleRate: 0.8,
-    config: {
-      // Add native Sentry config here
-      // https://docs.sentry.io/platforms/javascript/guides/vue/configuration/options/
-    }
-  },
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
 
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    treeShake: true,
-    optionsPath: './vuetify.options.js',
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+    googleAnalytics: {
+        // Options
+        id: 'G-C242Q1FLG1'
+    },
 }
