568433da2f6353b148a66f33e8fb9dd2ebbfd3ae
RenFei <i@renfei.net>
2022-07-15 14:57:09 +0800

    feat(ui): 浏览器控制台增加个性输出

--- renfeid-ui/pages/_app.tsx
+++ renfeid-ui/pages/_app.tsx
@@ -1,6 +1,6 @@
 import 'antd/dist/antd.css'
 import '../styles/globals.css'
-import {ReactNode} from 'react';
+import React, {ReactNode, useEffect, useState} from 'react';
 import {NextPage} from "next";
 import Head from 'next/head'
 import type {AppProps} from 'next/app'
@@ -15,6 +15,16 @@ type Props = AppProps & {
 
 const App = ({Component, pageProps}: Props) => {
     const getLayout = Component.getLayout ?? ((page: ReactNode) => page)
+    const [consoleLog, setConsoleLog] = useState<number>(0)
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            if (consoleLog == 0) {
+                console.log("\n %c RENFEI.NET %c 这里你都能找到？你知道的太多了！ %c \n\n", "color: #fff; background: #3274ff; padding:5px 0; border: 1px solid #3274ff;", "color: #3274ff; background: #fff; padding:5px 0; border: 1px solid #3274ff;", "");
+                console.log("\n %c 代码已开源： %c github.com/renfei/renfeid %c \n\n", "color: #fff; background: #2da44e; padding:5px 0; border: 1px solid #2da44e;", "color: #2da44e; background: #fff; padding:5px 0; border: 1px solid #2da44e;", "");
+                setConsoleLog(1)
+            }
+        }
+    }, [consoleLog])
     return getLayout(
         <>
             <Head>
