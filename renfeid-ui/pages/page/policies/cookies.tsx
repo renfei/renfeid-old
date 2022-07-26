5e65c6ae6cdc76d8f391f252291ba0904463b6af
RenFei <i@renfei.net>
2022-07-26 08:53:02 +0800

    refactor(ui): update pages

--- renfeid-ui/pages/page/policies/cookies.tsx
+++ renfeid-ui/pages/page/policies/cookies.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import {Alert} from 'antd'
 import Layout from '../../../components/layout'
 import styles from '../../../styles/Page.module.css'
 
@@ -12,7 +13,14 @@ const CookiesPoliciesPage = () => {
 
             <main className={styles.main}>
                 <section className={"renfeid-content"}>
-                    section
+                    <div style={{padding: '20px 0'}}>
+                        <Alert
+                            message="此部分闭源"
+                            description="该页面位于私有仓库闭源，在此处只做占位案例，你可以根据文件位置关系，创建自己的页面。"
+                            type="info"
+                            showIcon
+                        />
+                    </div>
                 </section>
             </main>
         </>
