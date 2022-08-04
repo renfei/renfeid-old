5d6819ffd2a499c394728f9874c4aea62f526ec5
RenFei <i@renfei.net>
2022-08-04 21:41:22 +0800

    feat(ui): 添加更多页面

---
+++ renfeid-ui/pages/more.tsx
@@ -0,0 +1,151 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import Layout from '../components/layout'
+import { Row, Col, Avatar, Card } from 'antd'
+
+const { Meta } = Card
+
+const MetaPageColStyle = {
+  padding: '10px'
+}
+
+const MorePage = () => {
+  return (
+    <>
+      <Head>
+        <title>更多信息 - {process.env.NEXT_PUBLIC_RENFEID_SITE_NAME}</title>
+        <meta name="description" content={`关于${process.env.NEXT_PUBLIC_RENFEID_SITE_NAME}`} />
+      </Head>
+
+      <main style={{ backgroundColor: '#ffffff' }}>
+        <section className={"renfeid-content"} style={{ paddingTop: '20px', paddingBottom: '20px' }}>
+          <Row style={{ padding: '20px 0' }}>
+            <Col xs={24} sm={12} md={8} lg={6} style={MetaPageColStyle}>
+              <Link href={'/page/about'}>
+                <a>
+                  <Card
+                    style={{ width: '100%' }}
+                    cover={
+                      <Image
+                        width={658}
+                        height={400}
+                        alt={`关于${process.env.NEXT_PUBLIC_RENFEID_SITE_NAME}`}
+                        src="https://cdn.renfei.net/renfeid/img/aboutimg.webp"
+                      />
+                    }
+                  >
+                    <Meta
+                      title={`关于${process.env.NEXT_PUBLIC_RENFEID_SITE_NAME}`}
+                      description={`关于${process.env.NEXT_PUBLIC_RENFEID_SITE_NAME}更多信息，联系方式与我的历史等信息。`}
+                    />
+                  </Card>
+                </a>
+              </Link>
+            </Col>
+            <Col xs={24} sm={12} md={8} lg={6} style={MetaPageColStyle}>
+              <Card
+                style={{ width: '100%' }}
+                cover={
+                  <Image
+                    width={658}
+                    height={400}
+                    alt="example"
+                    src="https://cdn.renfei.net/renfeid/img/aboutimg.webp"
+                  />
+                }
+              >
+                <Meta
+                  title="占位"
+                  description="你可以替换成你的卡片。"
+                />
+              </Card>
+            </Col>
+            <Col xs={24} sm={12} md={8} lg={6} style={MetaPageColStyle}>
+              <Card
+                style={{ width: '100%' }}
+                cover={
+                  <Image
+                    width={658}
+                    height={400}
+                    alt="example"
+                    src="https://cdn.renfei.net/renfeid/img/aboutimg.webp"
+                  />
+                }
+              >
+                <Meta
+                  title="占位"
+                  description="你可以替换成你的卡片。"
+                />
+              </Card>
+            </Col>
+            <Col xs={24} sm={12} md={8} lg={6} style={MetaPageColStyle}>
+              <Card
+                style={{ width: '100%' }}
+                cover={
+                  <Image
+                    width={658}
+                    height={400}
+                    alt="example"
+                    src="https://cdn.renfei.net/renfeid/img/aboutimg.webp"
+                  />
+                }
+              >
+                <Meta
+                  title="占位"
+                  description="你可以替换成你的卡片。"
+                />
+              </Card>
+            </Col>
+            <Col xs={24} sm={12} md={8} lg={6} style={MetaPageColStyle}>
+              <Card
+                style={{ width: '100%' }}
+                cover={
+                  <Image
+                    width={658}
+                    height={400}
+                    alt="example"
+                    src="https://cdn.renfei.net/renfeid/img/aboutimg.webp"
+                  />
+                }
+              >
+                <Meta
+                  title="占位"
+                  description="你可以替换成你的卡片。"
+                />
+              </Card>
+            </Col>
+            <Col xs={24} sm={12} md={8} lg={6} style={MetaPageColStyle}>
+              <Card
+                style={{ width: '100%' }}
+                cover={
+                  <Image
+                    width={658}
+                    height={400}
+                    alt="example"
+                    src="https://cdn.renfei.net/renfeid/img/aboutimg.webp"
+                  />
+                }
+              >
+                <Meta
+                  title="占位"
+                  description="你可以替换成你的卡片。"
+                />
+              </Card>
+            </Col>
+          </Row>
+        </section>
+      </main>
+    </>
+  )
+}
+
+MorePage.getLayout = (page: any) => {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}
+
+export default MorePage
