20587f5ca21a3a91b5bcad258aeacee0ab899453
RenFei <i@renfei.net>
2022-12-27 10:32:03 +0800

    ui: 移除前端的百度统计代码

--- renfeid-ui/pages/_app.tsx
+++ renfeid-ui/pages/_app.tsx
@@ -22,14 +22,6 @@ const getAnalyticsTag = () => {
         gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
           page_path: window.location.pathname,
         });
-
-        var _hmt = _hmt || [];
-        (function() {
-          var hm = document.createElement("script");
-          hm.src = "https://hm.baidu.com/hm.js?${process.env.NEXT_PUBLIC_BAIDU_TONGJI}";
-          var s = document.getElementsByTagName("script")[0];
-          s.parentNode.insertBefore(hm, s);
-        })();
         `,
     }
 }
